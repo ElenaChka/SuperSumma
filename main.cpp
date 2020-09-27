3f683fed45de7abaa5702844a350ed0fd9fcf1ac
ElenaChka <checkalina.lena@bk.ru>
2020-09-27 20:36:04 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ double a,b,sum=0;
+
+    cout<<"Vvedite pervoe chislo: ";
+   cin>>a;
+   cout<<"Vvedite vtoroe chislo: ";
+   cin>>b;
+   sum=a+b;
+   cout<<"Summa ravna "<<sum<<endl;
+}
